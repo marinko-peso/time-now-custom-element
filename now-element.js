cf9e2e966c76ec88811e50f777e9e1ab0ac48384
Marinko Peso <marinko.peso@gmail.com>
2018-10-07 01:07:31 +0200

    add initial time format :goat:

--- now-element.js
+++ now-element.js
@@ -14,8 +14,20 @@ class TimeNowElement extends HTMLElement {
     }
 
     timePolling() {
-        this.innerHTML = new Date();
+        this.innerHTML = this.formattedTime;
         this.pollingObj = setTimeout(() => this.timePolling(), 1000);
+    }
+
+    get formattedTime() {
+        const d = new Date();
+        const year = d.getFullYear();
+        const monthNum = d.getMonth().toString().padStart(2, '0');
+        const dayNum = d.getDate().toString().padStart(2, '0');
+        const hours = d.getHours().toString().padStart(2, '0');
+        const minutes = d.getMinutes().toString().padStart(2, '0');
+        const seconds = d.getSeconds().toString().padStart(2, '0');
+
+        return `${year}-${monthNum}-${dayNum} ${hours}:${minutes}:${seconds}`;
     }
 }
 
